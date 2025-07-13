95f4a69620fff3185df67317369ce125bf72ff43
roguetitan1703 <omchandel1703@gmail.com>
2025-07-14 00:27:56 +0530

    .

--- src/config/products.js
+++ src/config/products.js
@@ -5,26 +5,26 @@ module.exports = [
   {
     // Balatro EU
     cwsProductId: "3d11fc79-1e20-41f4-926f-4bcca5c9a579",
-    g2aOfferId: "i10000503618004",
+    g2aProductId: "i10000503618004",
   },
   {
     // Conan Exiles (Steam)
     cwsProductId: "4dd93973-8b02-465d-a323-a86d07d256b9",
-    g2aOfferId: "i10000033297006",
+    g2aProductId: "i10000033297006",
   },
   {
     // Europa Universalis IV 4
     cwsProductId: "fdab2bf5-7c11-42ec-8808-0093dcc222bd",
-    g2aOfferId: "i10000014547006",
+    g2aProductId: "i10000014547006",
   },
   {
     // Ancestors: The Humankind Odyssey
     cwsProductId: "c08e582f-edef-4936-86eb-2b1775711557",
-    g2aOfferId: "i10000189712013",
+    g2aProductId: "i10000189712013",
   },
   {
     // Cities: Skylines (Standard Edition)
     cwsProductId: "14e7693f-888e-470f-94f4-f102aa314d22",
-    g2aOfferId: "i10000005633009",
+    g2aProductId: "i10000005633009",
   },
 ];
