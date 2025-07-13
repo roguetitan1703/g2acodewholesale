9f88fe0af50674bb3f0cb914e13b4c0b452f0b11
Ashackq <akashpatelyo2@gmail.com>
2025-07-14 01:28:56 +0530

    [update] notes for workflow 1 postman collection product to offer id mapping

--- src/config/products.js
+++ src/config/products.js
@@ -5,26 +5,36 @@ module.exports = [
   {
     // Balatro EU
     cwsProductId: "3d11fc79-1e20-41f4-926f-4bcca5c9a579",
-    g2aProductId: "i10000503618004",
+    g2aProductId: "10000503618004",
+    g2aOfferId: "71f70676-ff89-4684-b792-a7d2b864da89",
+
   },
   {
     // Conan Exiles (Steam)
     cwsProductId: "4dd93973-8b02-465d-a323-a86d07d256b9",
-    g2aProductId: "i10000033297006",
+    g2aProductId: "10000033297006",
+    g2aOfferId: "5eaf755b-4ffc-4678-8a75-5c73c03bce18",
+
   },
   {
     // Europa Universalis IV 4
     cwsProductId: "fdab2bf5-7c11-42ec-8808-0093dcc222bd",
-    g2aProductId: "i10000014547006",
+    g2aProductId: "10000014547006",
+    g2aOfferId: "765bbb5c-73b0-49e1-a3b7-2788a36b2706",
+
   },
   {
     // Ancestors: The Humankind Odyssey
     cwsProductId: "c08e582f-edef-4936-86eb-2b1775711557",
-    g2aProductId: "i10000189712013",
+    g2aProductId: "10000189712013",
+    g2aOfferId: "3ec42001-8f7c-418c-b7ef-2a674ace27b6",
+
   },
   {
     // Cities: Skylines (Standard Edition)
     cwsProductId: "14e7693f-888e-470f-94f4-f102aa314d22",
-    g2aProductId: "i10000005633009",
+    g2aProductId: "10000005633009",
+    g2aOfferId: "e8979d78-1a1e-4865-8a92-d6d7d7eb12ce",
+
   },
 ];
